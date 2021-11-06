34246dd1ec2a9da8123d2511970d12350cb0dbb5
Llewellyn Dsouza <lledsouza2209@gmail.com>
2021-11-06 14:41:32 +0530

    Added minor animations

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,10 +1,16 @@
 import styles from "../../styles/Header.module.css";
+import { motion } from "framer-motion";
 
 export default function Header() {
   return (
     <header>
-      <div className={styles.leftHeader}>{"Llewellyn D'souza"}</div>
-      <div className={styles.rightHeader}>Welcome to my page</div>
+      <div className={styles.leftHeader}>
+        <motion.div initial={{ y: -100 }} animate={{ y: 0 }} transition={{type:"spring", damping: 9}}>
+          {"Llewellyn D'souza"}
+        </motion.div>
+        <hr />
+      </div>
+      {/* <div className={styles.rightHeader}>{" "}</div> */}
     </header>
   );
 }
