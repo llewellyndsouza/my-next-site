b28f9a27e52cd1422bb5349b4d15b2712033e926
Llewellyn Dsouza <lledsouza2209@gmail.com>
2021-11-12 23:31:36 +0530

    Phase 1 - name, tagline and links

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,24 @@
-import Layout from "../components/layout";
-import styles from "../styles/Home.module.css";
+import Layout from '../components/layout';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <Layout title="Home">
-      <section className={styles.content}>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus ultricies turpis id tincidunt. Donec in tortor eu quam congue
-          bibendum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec vel lectus eleifend, molestie
-          nisl a, pharetra sapien.{" "}
-        </p>
-        <p>
-          {" "}
-          Ut quis quam non ipsum posuere blandit sit amet posuere risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam
-          pellentesque arcu velit, sit amet fringilla turpis feugiat rutrum. Duis et ligula eu dolor accumsan viverra id a nibh.{" "}
-        </p>
-        <p>
-          Nunc hendrerit elit at nibh suscipit lacinia vitae eget erat. Integer nec tristique dui, eu semper est. Donec odio magna, eleifend vitae
-          velit eu, fermentum tincidunt velit.
-        </p>
-        <p> Integer pretium, nisl vitae venenatis vulputate, massa nisl mollis felis, in luctus ante magna vel risus.</p>
-      </section>
+    <Layout title="Full-stack Developer">
+      <div className={styles.intro}>
+        <div className={styles.title}>Llewellyn Dsouza</div>
+        <div className={styles.subtitle}>I am a Full-stack developer with a focus on backend technologies and I love trying out new things.</div>
+        <div className={styles.icons}>
+          <a href="https://github.com/llewellyndsouza" className={styles.icon}>
+            <img src="/github.svg" alt="Check out my git" width={48} height={48} />
+          </a>
+          <a href="mailto: lledsouza2209@gmail.com" className={styles.icon}>
+            <img src="/mail.svg" alt="Get in touch" width={48} height={48} />
+          </a>
+          {/* <a className={styles.icon}>
+            <img src="/linkedin.svg" alt="Notice me" width={48} height={48} />
+          </a> */}
+        </div>
+      </div>
     </Layout>
   );
 }
