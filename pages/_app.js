b8881597714d65f217d35b92a09cb90d65a069c1
Llewellyn Dsouza <lledsouza2209@gmail.com>
2021-11-14 15:43:48 +0530

    Added: Theme toggle switch

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
-import "../styles/globals.css";
+import { createContext, useState } from 'react';
+import '../styles/globals.css';
+
+export const ThemeContext = createContext({
+  dark: true,
+  setDark: (s) => !s
+});
 
 function MyApp({ Component, pageProps }) {
+  const [dark, setDark] = useState(false);
+  const values = { dark, setDark };
   // console.log("Hey there, looking for something? Feel free to shoot me a message :)");
-  return <Component {...pageProps} />;
+  return (
+    <ThemeContext.Provider value={values}>
+      <Component {...pageProps} />{' '}
+    </ThemeContext.Provider>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import Layout from '../components/layout';
 import styles from '../styles/Home.module.css';
+import { motion } from 'framer-motion';
+import { useContext, useState } from 'react';
+import { ThemeContext } from './_app';
+
+const spring = {
+  type: 'spring',
+  stiffness: 700,
+  damping: 30
+};
 
 export default function Home() {
+  const theme = useContext(ThemeContext);
+
+  const toggleTheme = () => {
+    theme.setDark((s) => !s);
+  };
+
   return (
     <Layout title="Full-stack Developer">
       <div className={styles.intro}>
@@ -12,6 +27,7 @@ export default function Home() {
           {'Llewellyn'}
         </div>
         <div className={styles.subtitle}>and I am a Full-stack developer with a focus on backend technologies and I love trying out new things.</div>
+        <div className={`${styles.icons} ${theme.dark ? styles.darkSelected : ''}`}>
           <a href="https://github.com/llewellyndsouza" className={styles.icon}>
             <img src="/github.svg" alt="Check out my git" width={48} height={48} />
           </a>
@@ -21,6 +37,12 @@ export default function Home() {
           {/* <a className={styles.icon}>
             <img src="/linkedin.svg" alt="Notice me" width={48} height={48} />
           </a> */}
+        </div>
+        <div className={styles.themeSelector}>
+          <div className={`${styles.themeSwitch} ${theme.dark ? styles.darkSelected : ''}`} onClick={toggleTheme}>
+            <motion.div className={`${styles.themeHandle} ${theme.dark ? styles.darkSelected : ''}`} layout transition={spring} />
+          </div>
+          <div className={styles.themeSelectorText}>{theme.dark ? 'Too light?' : 'Too bright?'}</div>
         </div>
       </div>
     </Layout>
